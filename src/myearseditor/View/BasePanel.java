7d49df12ddc3542e741f273d759c4b0d7b5ae246
krishbhask89 <krishbhask89@gmail.com>
2014-11-08 03:45:49 +0530

    eon...7/11

--- src/myearseditor/View/BasePanel.java
+++ src/myearseditor/View/BasePanel.java
@@ -5,6 +5,9 @@
  */
 package myearseditor.View;
 
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+
 /**
  *
  * @author Krish
@@ -44,7 +47,7 @@ public class BasePanel extends javax.swing.JPanel {
             .addGroup(_comboBoxPanelLayout.createSequentialGroup()
                 .addGap(145, 145, 145)
                 .addComponent(_reqTypeList, javax.swing.GroupLayout.PREFERRED_SIZE, 179, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(76, Short.MAX_VALUE))
+                .addContainerGap(146, Short.MAX_VALUE))
         );
         _comboBoxPanelLayout.setVerticalGroup(
             _comboBoxPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -65,12 +68,29 @@ public class BasePanel extends javax.swing.JPanel {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(_comboBoxPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(247, Short.MAX_VALUE))
+                .addContainerGap(298, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
     private void _reqTypeListActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event__reqTypeListActionPerformed
         // TODO add your handling code here:
+        int _reqSelected = _reqTypeList.getSelectedIndex();
+        if(_reqSelected > 0 && _reqSelected < 6)
+        {    JPanel _newReq = new JPanel(new BorderLayout());   
+             
+           switch(_reqSelected)
+           { case 1: JPanel _reqProps = new LeftPanel("Generic");
+                  JPanel _reqTemp = new GenericTemp();
+                  _newReq.add(_reqProps, BorderLayout.WEST);
+                  _newReq.add(_reqTemp,BorderLayout.EAST);
+                  break;
+             case 2: JPanel _reqProps = new LeftPanel("Ubiquitous");
+                  JPanel _reqTemp = new UbiTemp();
+                  _newReq.add(_reqProps, BorderLayout.WEST);
+                  _newReq.add(_reqTemp,BorderLayout.EAST);
+                  break;
+           }    
+        }    
     }//GEN-LAST:event__reqTypeListActionPerformed
 
 
